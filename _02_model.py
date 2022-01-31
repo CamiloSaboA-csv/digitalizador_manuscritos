70871c01474772913b7ad23d1d26f3169ac5663b
CamiloS_A <camilo.sabogal@eia.edu.co>
2022-01-31 11:39:26 -0500

    creacion del modelo

--- _02_model.py
+++ _02_model.py
@@ -1,1 +1,35 @@
-#######
+
+
+def model_creation():
+    model = tf.keras.models.Sequential([
+        #input shape is the desired size of the image 300x300 with 3 bytes color
+
+        # This is the first convolution
+        tf.keras.layers.Conv2D(16, (3,3), activation='relu', input_shape=(300, 300, 3)),
+        tf.keras.layers.MaxPooling2D(2, 2),
+
+        # The second convolution
+        tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+
+        # The third convolution
+        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+
+        # The fourth convolution
+        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+
+        # The fifth convolution
+        tf.keras.layers.Conv2D(64, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(2,2),
+
+        # Flatten the results to feed into a DNN
+        tf.keras.layers.Flatten(),
+
+        # 512 neuron hidden layer
+        tf.keras.layers.Dense(512, activation='relu'),
+
+        # 52 output neuron.
+        tf.keras.layers.Dense(52, activation='softmax')
+    ])
